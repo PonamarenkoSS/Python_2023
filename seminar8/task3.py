6aa5f930b9095e4692871b6728d7897f8843acff
Svetlana Ponamarenko <svetlanaponamarenko@MacBook-Pro-Svetlana.local>
2024-02-15 22:18:07 +0400

    done8_2

---
+++ seminar8/task3.py
@@ -0,0 +1,20 @@
+import json
+import csv 
+from pathlib import Path
+
+def json_to_csv(path: Path):
+    with open(path, 'r', encoding='UTF-8') as file_read:
+        data = json.load(file_read)
+    
+    result = []
+    for level, id_name in data.items():
+        for id, name in id_name.items():
+            result.append({'level': level, 'id': id, 'name': name})
+    with open(path.stem + '.csv', 'w', encoding='UTF-8', newline='') as file_write:
+        csv_write = csv.DictWriter(file_write, fieldnames=['level', 'id', 'name'], dialect='excel-tab')
+        csv_write.writeheader()
+        csv_write.writerows(result)
+
+
+if __name__ == '__main__':
+    json_to_csv(Path('users.json'))
